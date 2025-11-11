246083ee2a531c2a843febb602a9bb40ca0126e0
MuraliMohan2018 <38581517+MuraliMohan2018@users.noreply.github.com>
2025-11-11 06:25:00 +0530

    Update HelloWorld.java

--- HelloWorld.java
+++ HelloWorld.java
@@ -4,8 +4,10 @@ class HelloWorld
     // Prints "Hello, World" to the terminal window. 
     public static void main(String args[]) 
     { 
-        System.out.println("Welcome to Leena IT"); 
+        System.out.println("Welcome to Murali"); 
 		System.out.println("Thanks for joining the class");
 		System.out.println("We are learning about DevOps");
+		System.out.println("We are Students");
     } 
+
 } 
